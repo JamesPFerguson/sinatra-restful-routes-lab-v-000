abeaa678ff40c4a635cc3c42e5adeb4ceecaec51
James Ferguson <ncjamesferguson@gmail.com>
2018-02-20 18:03:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(params)
-    redirect to "/recipes/#{@recipe.id}"
+    redirect "/recipes/#{@recipe.id}"
   end
 
   patch '/recipes/:id' do
